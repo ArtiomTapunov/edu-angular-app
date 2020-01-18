814c04cea39b89873c451f2af8efbf858edad9aa
Artiom <artiom.tapunov@gmail.com>
2020-01-18 23:33:21 +0300

    Home component added.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,4 +1,4 @@
-import { LoginComponent } from './auth/login.component';
+import { LoginComponent } from './login/login.component';
 import { BasicLayoutComponent } from './layouts/basiclayout.component';
 import { NavigationComponent } from './layouts/navigation.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,13 +6,15 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
         NavigationComponent,
         BasicLayoutComponent,
         LoginComponent,
-        UsersComponent
+        UsersComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
