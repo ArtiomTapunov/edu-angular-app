0cb67459a7142285490bd4e3718ed3ab00465c01
Artiom <artiom.tapunov@gmail.com>
2020-01-18 14:34:38 +0300

    Login component added.

---
+++ src/app/components/auth/login.component.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../../services/auth.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: "logincomponent",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"]
+})
+
+export class LoginComponent {
+    public isLoading: boolean = false;
+    public email: string;
+    public password: string;
+
+    constructor (
+        private authService: AuthService,
+        private router: Router) {
+
+    }
+
+    login() {
+        this.authService.authenticate(this.email, this.password).subscribe(x => {
+            sessionStorage.setItem("token", x.access_token);
+            this.router.navigate(["users"]);
+        })
+    }
+}
