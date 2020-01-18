c85b79761a9967b6364e30cd1a7121f271c513a4
Artiom <artiom.tapunov@gmail.com>
2020-01-18 14:31:01 +0300

    Initial angular app has been created!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/auth/login.component';
+import { BasicLayoutComponent } from './components/layouts/basiclayout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login"
+  },
+  {
+    path: "",
+    component: BasicLayoutComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginComponent
+      }
+    ]
+  },
+  {
+    path: "users",
+    component: BasicLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: UsersComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { UserManagementService } from './services/usermanagement.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './share/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    UserManagementService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
