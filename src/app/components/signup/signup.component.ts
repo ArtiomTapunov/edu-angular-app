737f3d0cec074cb94b2919eb0bb4f9ad24030189
Atiom <artiom.tapunov@gmail.com>
2020-01-26 16:43:20 +0300

    Added error handling in the Sign Up component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserModel } from 'src/app/models/user.model';
 import { Router } from '@angular/router';
 import { UserExtendedModel } from 'src/app/models/userextended.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'signup',
@@ -13,6 +14,8 @@ export class SignUpComponent implements OnInit {
   public isLoading: boolean = false;
   public registerUserModel: UserExtendedModel;
   public user: UserModel;
+  public errorMessage: string;
+  public isError: boolean;
 
   constructor(
     private authService: AuthService,
@@ -21,17 +24,27 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit() {
     this.registerUserModel = {};
+    this.isError = false;
   }
 
   signUp() {
-    this.authService.register(this.registerUserModel).subscribe(x => {
+    this.authService.register(this.registerUserModel).subscribe(
+      x => {
       this.user = x.Data;
 
       //Processing user logic can be added here
       //console.log(this.user);
 
       this.router.navigate(["login"]);
-  })
+      },
+      (error: any) => {
+        if(error)
+        {
+          this.isError = true;
+          this.errorMessage = error;
+        }
+      }
+    )
   }
 
 }
