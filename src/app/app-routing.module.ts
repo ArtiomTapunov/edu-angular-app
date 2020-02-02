e110d04dadb9050b71023d81a5959bd94cec53e6
Atiom <artiom.tapunov@gmail.com>
2020-02-02 14:31:12 +0300

    App-routing refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,13 +25,7 @@ const routes: Routes = [
         path: "login",
         component: LoginComponent,
         canActivate: [LoginGuard]
-      }
-    ]
-  },
-  {
-    path: "",
-    component: BasicLayoutComponent,
-    children: [
+      },
       {
         path: "signup",
         component: SignUpComponent,
