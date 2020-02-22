98942d2adc383fd76a0f3f79cb2b46ab45e65339
Artiom <artiom.tapunov@gmail.com>
2020-02-22 23:48:46 +0300

    Background added

--- src/app/components/layouts/basiclayout.component.ts
+++ src/app/components/layouts/basiclayout.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "basiclayout",
-    templateUrl: "./basiclayout.component.html"
+    templateUrl: "./basiclayout.component.html",
+    styleUrls: ['./basiclayout.component.scss']
 })
 
 export class BasicLayoutComponent {
